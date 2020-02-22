b14555f741bbe3f04bc1dfe484b2f56b57bfd6ec
Kento Nishi <kento24gs@outlook.com>
2020-02-21 19:36:02 -0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello, World!"
+
+
+@app.route("/api", methods=["POST"])
+def get_tasks():
+    return jsonify({"hello": "world"})
+
+
+if __name__ == "__main__":
+    app.run()
